f0b4d27620afdfed36d3c2a8e65027aecce11327
Martin Doležal <dolezalmart@gmail.com>
2022-09-12 20:56:05 +0200

    Refactor code - pandas methods

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 from tkinter import filedialog
 import pandas as pd
-from time import strptime
 import matplotlib.pyplot as plt
-import datetime
-from pytz import timezone
-import pytz
 import numpy as np
 
 if __name__ == '__main__':
@@ -13,22 +9,23 @@ if __name__ == '__main__':
     data = pd.read_csv(f"{directory}\\CONTENT_INTERACTION\\ViewingActivity.csv")
     # remove irrelevant rows - trailers, hooks
     data = data[data["Supplemental Video Type"].isna()]
-    start_times = pd.array(data["Start Time"])
+    data = data[data["Profile Name"] == "Martin"]
+    data["Duration"] = pd.to_timedelta(data["Duration"])
+    data["Start Time"] = pd.to_datetime(data["Start Time"], format="%Y-%m-%d %H:%M:%S")
 
-    # convert times from string to datetime object
-    hours_utc = [datetime.datetime(*strptime(item, "%Y-%m-%d %H:%M:%S")[:6]) for item in start_times]
-
+    durations = pd.array(data["Duration"])
+    data["Stop Time"] = data["Start Time"] + data["Duration"]
     # times are in utc, need to be converted to local time TODO change to any local time, not hardcoded Prague,
     #  maybe argument
-    hours_prague = [pytz.utc.localize(hour, is_dst=True).astimezone(timezone("Europe/Prague")).hour for hour in
-                    hours_utc]
+    data["Start Time"] = data["Start Time"].dt.tz_localize("UCT").dt.tz_convert("CET")
+    data["Stop Time"] = data["Stop Time"].dt.tz_localize("UCT").dt.tz_convert("CET")
 
-    # arrays for plotting
+    # lists for plotting
     hour_count = []
     day_hours = []
     for hour in range(24):
         day_hours.append(hour)
-        hour_count.append(hours_prague.count(hour))
+        hour_count.append(list(data["Start Time"].dt.hour).count(hour))
     fig, ax = plt.subplots(layout="constrained")
     ax.bar(day_hours, hour_count)
     ax.set_title("Netflix starting times grouped by hour")
